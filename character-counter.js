37a5bd449a65d94d9eae3deb20c2e32a128aa0ff
Brainy Cat <Sergiosanta19@gmail.com>
2018-09-11 20:55:04 +0200

    Add files via upload

---
+++ character-counter.js
@@ -0,0 +1,30 @@
+/*
+*   Title: Character-counter.js
+*	Desc:  Counts number of characters in a text field in HTML
+*	@author: https://github.com/BrainyCat/
+*/
+document.addEventListener("DOMContentLoaded", function(event) {
+	var supportOnInput = 'oninput' in document.createElement('input');
+	var CSScolor =  getComputedStyle(document.body).getPropertyValue('color').toString();
+
+	$("#messagebody[maxlength]").each(function() {
+		var $this = $(this);
+		var maxLength = parseInt($this.attr('maxlength'));
+		$this.attr('maxlength', null);
+		
+		var el = $("<span class=\"character-count\">" + maxLength + "</span>");
+		el.insertAfter($this);
+		
+		$this.bind(supportOnInput ? 'input' : 'keyup', function() {
+			var cc = $this.val().length;
+			
+			el.text(maxLength - cc);
+			
+			if(maxLength < cc) {
+				el.css('color', 'red');
+			} else {
+				el.css('color', CSScolor);
+			}
+		});
+	});
+});
